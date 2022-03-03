a5d563eac3e82fd3c19df9052a3763d3b4197daa
Krishna Gunjal <kgunjal51@gmail.com>
2022-03-03 16:50:14 +0530

    Initial commit

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Krishna Gunjal on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
