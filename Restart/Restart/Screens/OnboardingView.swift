e6fd2b3dc9f3ed612aa3a67170c00788f9747163
Krishna Gunjal <kgunjal51@gmail.com>
2022-03-04 20:23:15 +0530

    Added arrow to give hint user for image is draggable and small animation add on.

--- Restart/Restart/Screens/OnboardingView.swift
+++ Restart/Restart/Screens/OnboardingView.swift
@@ -13,6 +13,8 @@ struct OnboardingView: View {
     @State private var buttonOffset: CGFloat = 0
     @State private var isAnimating: Bool = false
     @State private var imageOffset: CGSize = .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share."
     
     var body: some View {
         ZStack {
@@ -24,10 +26,11 @@ struct OnboardingView: View {
                 Spacer()
                 
                 VStack(spacing: 0){
-                    Text("Share.")
+                    Text(textTitle)
                         .font(.system(size: 60))
                         .fontWeight(.heavy)
                         .foregroundColor(.white)
+                        .id(textTitle)
                     
                     Text("""
                     Its not how much we give but
@@ -63,14 +66,34 @@ struct OnboardingView: View {
                             .onChanged{ gesture in
                                 if abs(imageOffset.width) <= 150 {
                                     imageOffset = gesture.translation
+                               
+                                    withAnimation(.linear(duration: 0.25)){
+                                        indicatorOpacity = 0
+                                        textTitle = "Give."
+                                    }
                                 }
                             }
                             .onEnded{ _ in
                                 imageOffset = .zero
+                                
+                                withAnimation(.linear(duration: 0.25)){
+                                    indicatorOpacity = 1
+                                    textTitle = "Share."
+                                }
                             }
                         )
                         .animation(.easeOut(duration: 1), value: imageOffset)
                 }
+                .overlay(
+                Image(systemName: "arrow.left.and.right.circle")
+                    .font(.system(size: 44, weight: .ultraLight))
+                    .foregroundColor(.white)
+                    .offset(y: 20)
+                    .opacity(isAnimating ? 1 : 0)
+                    .animation(.easeOut(duration: 1).delay(2), value: isAnimating)
+                    .opacity(indicatorOpacity)
+                , alignment: .bottom
+                )
                 Spacer()
                 
                 //MARK: - FOOTER
